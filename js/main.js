40ed4ee2e0ce6150640fa969bbaf1a5fac1c3cee
Inna Storozhuk <istorozuk@yandex.ru>
2021-01-12 17:28:48 +0100

    funkcje - to kody zadań

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 5;
+
+let secondNumber = 2;
+
+function suma (firstNumber, secondNumber) {
+    let result = firstNumber+secondNumber;
+    return result;
+}
+console.log(suma (5,2));
+let wynik = suma (5, 2)
